0342a178dd6936312a7fbc75c2682b1bf18f2738
alexgul25 <fine.9ulyaev@yandex.ru>
2026-06-25 15:07:38 +0300

    add logger and jwt parsing

--- go.mod
+++ go.mod
@@ -6,3 +6,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/kelseyhightower/envconfig v1.4.0
 )
+
+require github.com/lmittmann/tint v1.1.3
+
+require github.com/golang-jwt/jwt/v5 v5.3.1
---
+++ internal/lib/jwt/jwt.go
@@ -0,0 +1,34 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var (
+	ErrSigningMethod = errors.New("unexpected signing method")
+	ErrParseToken    = errors.New("failed to parse token")
+)
+
+type TokenClaims struct {
+	jwt.RegisteredClaims
+	UserID string `json:"user_id"`
+}
+
+func ParseToken(tokenStr string, secret []byte) (*TokenClaims, error) {
+	var claims TokenClaims
+	_, err := jwt.ParseWithClaims(tokenStr, &claims, func(t *jwt.Token) (any, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, ErrSigningMethod
+		}
+		return secret, nil
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("%w: %w", ErrParseToken, err)
+	}
+
+	return &claims, nil
+}
